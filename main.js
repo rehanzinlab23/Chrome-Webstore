7be9bf3db30ec6cd6764db52e78b03560fa15ce0
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-10-31 17:39:09 +0500

    all the stylings are done expect slider

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 let swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       centeredSlides: true,
-      autoplay: {
-        delay: 25000,
-        disableOnInteraction: false,
-      },
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
          pagination: {
         el: ".swiper-pagination",
         type: "fraction",
       },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+        loop: true,
+    navigation: {
+      nextEl: "#nextBtn",
+      prevEl: "#prevBtn", 
+    },
     });
+
+      // Play/Pause Button Logic
+
+  const playPauseBtn = document.getElementById("playPauseBtn");
+  let isPlaying = true;
+
+  playPauseBtn.addEventListener("click", () => {
+    if (isPlaying) {
+      swiper.autoplay.stop(); // ⏸ Stop autoplay
+      playPauseBtn.innerHTML = `<svg enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" class=" NMm5M"><g><rect fill="none" height="24" width="24"></rect></g><g><g><path d="M6,19h4V5H6V19z M14,5v14h4V5H14z"></path></g></g></svg>`;
+
+    } else {
+      swiper.autoplay.start(); // ▶ Start autoplay
+      playPauseBtn.innerHTML = `<svg enable-background="new 0 0 24 24" focusable="false" height="24" viewBox="0 0 24 24" width="24" class=" NMm5M"><g><rect fill="none" height="24" width="24"></rect></g><g><g><path d="M8,5v14l11-7L8,5z"></path></g></g></svg>`;
+    }
+    isPlaying = !isPlaying;
+  });

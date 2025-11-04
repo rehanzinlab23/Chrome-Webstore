c797c5e22c03a808a23fdb72e389c33227d4a995
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-11-04 15:43:13 +0500

    home page done

--- main.js
+++ main.js
@@ -122,7 +122,24 @@ document.addEventListener('DOMContentLoaded', () => {
 const threeDots = document.querySelector('.three-dots');
 const menu = document.querySelector('.context-menu-container');
 
-threeDots.addEventListener('click', () => {
-  menu.style.display = "flex";
+threeDots.addEventListener('click', (e) => {
+  e.stopPropagation();
+  menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
 });
+
+document.body.addEventListener('click', () => {
+  menu.style.display = 'none';
+});
+});
+
+// Scroll Effect
+
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
